c5f8161dfe2b2d586b23319d714d079a422a04db
varoOP <46842159+varoOP@users.noreply.github.com>
2023-03-17 09:47:23 +0530

    Add: Example config

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,6 @@ import (
 type Config struct {
 	Dsn       string
 	Config    string
-	Token     string
 	Log       string
 	Addr      string
 	User      string
@@ -39,8 +38,14 @@ func NewConfig(dir string) *Config {
 	dsn := filepath.Join(dir, "shinkuro.db")
 	c.Dsn = fmt.Sprintf("file:%v?cache=shared&mode=rwc&_journal_mode=WAL", dsn)
 	c.Config = filepath.Join(dir, "config.toml")
-	c.Token = filepath.Join(dir, "token.json")
 	c.Log = filepath.Join(dir, "shinkuro.log")
+
+	if _, err = os.Stat(c.Config); err != nil {
+		createConfig(c)
+		log.Println("Example config.toml created at", c.Config)
+		log.Println("Edit before running shinkuro or shinkuro malauth again!")
+		os.Exit(0)
+	}
 
 	if err := c.K.Load(file.Provider(c.Config), toml.Parser()); err != nil {
 		log.Fatalf("Error loading configuration: %v", err)
@@ -68,6 +73,36 @@ func NewConfig(dir string) *Config {
 	mw := io.MultiWriter(os.Stdout, c.Logger)
 
 	log.SetOutput(mw)
-
 	return c
+}
+
+func createConfig(cfg *Config) {
+	const config = `###Sample shinkuro config
+
+host = "127.0.0.1"
+
+port = 7011
+
+plex_user = "Your_Plex_account_Title_EDIT_REQUIRED" 
+
+############################################
+#Default base_url = "/" (Optional setting) #
+############################################
+#base_url = "/shinkuro"
+
+##########################################################
+#Default is set to the community map. (Optional setting) #
+##########################################################
+#custom_map = "Absolute path to custom-mapping.yaml"
+`
+	f, err := os.Create(cfg.Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 }

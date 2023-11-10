a5c0abea08231094c41d68eb669d5d4e35ec2509
Rohit Vardam <46842159+varoOP@users.noreply.github.com>
2023-11-09 22:31:41 -0800

    fix(tautulli): accept multiple guids (#38)

--- internal/tautulli/tautulli.go
+++ internal/tautulli/tautulli.go
@@ -12,14 +12,14 @@ type Tautulli struct {
 		Title string `json:"title"`
 	} `json:"Account"`
 	Metadata struct {
-		GrandparentKey      string `json:"grandparentKey"`
-		GrandparentTitle    string `json:"grandparentTitle"`
-		GUID                string `json:"guid"`
-		Index               string `json:"index"`
-		LibrarySectionTitle string `json:"librarySectionTitle"`
-		ParentIndex         string `json:"parentIndex"`
-		Title               string `json:"title"`
-		Type                string `json:"type"`
+		GrandparentKey      string    `json:"grandparentKey"`
+		GrandparentTitle    string    `json:"grandparentTitle"`
+		GUID                plex.GUID `json:"guid"`
+		Index               string    `json:"index"`
+		LibrarySectionTitle string    `json:"librarySectionTitle"`
+		ParentIndex         string    `json:"parentIndex"`
+		Title               string    `json:"title"`
+		Type                string    `json:"type"`
 	} `json:"Metadata"`
 	Event string `json:"event"`
 }
@@ -60,11 +60,9 @@ func ToPlex(b []byte) (*plex.PlexWebhook, error) {
 			Title: t.Account.Title,
 		},
 		Metadata: plex.Metadata{
-			GrandparentKey:   t.Metadata.GrandparentKey,
-			GrandparentTitle: t.Metadata.GrandparentTitle,
-			GUID: plex.GUID{
-				GUID: t.Metadata.GUID,
-			},
+			GrandparentKey:      t.Metadata.GrandparentKey,
+			GrandparentTitle:    t.Metadata.GrandparentTitle,
+			GUID:                t.Metadata.GUID,
 			Index:               index,
 			LibrarySectionTitle: t.Metadata.LibrarySectionTitle,
 			ParentIndex:         parentIndex,

4a217cb6202bdfbb311e4e09bb330585c37d46af
varoOP <46842159+varoOP@users.noreply.github.com>
2023-12-26 02:57:03 -0800

    refactor(animeupdate): UpdateAnimeList

--- internal/animeupdate/service.go
+++ internal/animeupdate/service.go
@@ -3,6 +3,7 @@ package animeupdate
 import (
 	"context"
 
+	"github.com/nstratos/go-myanimelist/mal"
 	"github.com/rs/zerolog"
 	"github.com/varoOP/shinkro/internal/anime"
 	"github.com/varoOP/shinkro/internal/domain"
@@ -43,157 +44,79 @@ func (s *service) GetByID(ctx context.Context, req *domain.GetAnimeUpdateRequest
 }
 
 func (s *service) UpdateAnimeList(ctx context.Context, anime *domain.AnimeUpdate, event domain.PlexEvent) error {
-	switch event {
-	case domain.PlexRateEvent:
-		if anime.SourceDB == domain.MAL {
-			anime.MALId = anime.SourceId
-			client, err := s.malauthService.GetMalClient(ctx)
-			if err != nil {
-				return err
-			}
-
-			err = anime.UpdateRating(ctx, client)
-			if err != nil {
-				return err
-			}
-
-			err = s.Store(ctx, anime)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		animeMap, err := s.mapService.CheckForAnimeinMap(ctx, anime)
-		if err == nil {
-			anime.MALId = animeMap.Malid
-			client, err := s.malauthService.GetMalClient(ctx)
-			if err != nil {
-				return err
-			}
-			err = anime.UpdateRating(ctx, client)
-			if err != nil {
-				return err
-			}
-
-			err = s.Store(ctx, anime)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		if anime.SeasonNum == 1 {
-			req := &domain.GetAnimeRequest{
-				IDtype: anime.SourceDB,
-				Id:     anime.SourceId,
-			}
-
-			animeFromDB, err := s.animeService.GetByID(ctx, req)
-			if err != nil {
-				return err
-			}
-
-			anime.MALId = animeFromDB.MALId
-			client, err := s.malauthService.GetMalClient(ctx)
-			if err != nil {
-				return err
-			}
-
-			err = anime.UpdateRating(ctx, client)
-			if err != nil {
-				return err
-			}
-
-			err = s.Store(ctx, anime)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-	case domain.PlexScrobbleEvent:
-		if anime.SourceDB == domain.MAL {
-			anime.MALId = anime.SourceId
-			client, err := s.malauthService.GetMalClient(ctx)
-			if err != nil {
-				return err
-			}
-
-			done, err := anime.UpdateWatchStatus(ctx, client)
-			if err != nil {
-				return err
-			}
-
-			if done {
-				err = s.Store(ctx, anime)
-				if err != nil {
-					return err
-				}
-			}
-
-			return nil
-		}
-
-		animeMap, err := s.mapService.CheckForAnimeinMap(ctx, anime)
-		if err == nil {
-			anime.MALId = animeMap.Malid
-			anime.EpisodeNum = animeMap.CalculateEpNum(anime.EpisodeNum)
-			client, err := s.malauthService.GetMalClient(ctx)
-			if err != nil {
-				return err
-			}
-			done, err := anime.UpdateWatchStatus(ctx, client)
-			if err != nil {
-				return err
-			}
-
-			if done {
-				err = s.Store(ctx, anime)
-				if err != nil {
-					return err
-				}
-			}
-
-			return nil
-		}
-
-		if anime.SeasonNum == 1 {
-			req := &domain.GetAnimeRequest{
-				IDtype: anime.SourceDB,
-				Id:     anime.SourceId,
-			}
-
-			animeFromDB, err := s.animeService.GetByID(ctx, req)
-			if err != nil {
-				return err
-			}
-
-			anime.MALId = animeFromDB.MALId
-			client, err := s.malauthService.GetMalClient(ctx)
-			if err != nil {
-				return err
-			}
-
-			done, err := anime.UpdateWatchStatus(ctx, client)
-			if err != nil {
-				return err
-			}
-
-			if done {
-				err = s.Store(ctx, anime)
-				if err != nil {
-					return err
-				}
-			}
-
-			return nil
-		}
-
-	}
-
-	return nil
+    switch event {
+    case domain.PlexRateEvent:
+        return s.handleRateEvent(ctx, anime)
+    case domain.PlexScrobbleEvent:
+        return s.handleScrobbleEvent(ctx, anime)
+    }
+    return nil
 }
+
+func (s *service) handleRateEvent(ctx context.Context, anime *domain.AnimeUpdate) error {
+    if anime.SourceDB == domain.MAL {
+        anime.MALId = anime.SourceId
+        return s.updateAndStore(ctx, anime, anime.UpdateRating)
+    }
+
+    animeMap, err := s.mapService.CheckForAnimeinMap(ctx, anime)
+    if err == nil {
+        anime.MALId = animeMap.Malid
+        return s.updateAndStore(ctx, anime, anime.UpdateRating)
+    }
+
+    if anime.SeasonNum == 1 {
+        return s.updateFromDBAndStore(ctx, anime, anime.UpdateRating)
+    }
+
+    return err
+}
+
+func (s *service) handleScrobbleEvent(ctx context.Context, anime *domain.AnimeUpdate) error {
+    if anime.SourceDB == domain.MAL {
+        anime.MALId = anime.SourceId
+        return s.updateAndStore(ctx, anime, anime.UpdateWatchStatus)
+    }
+
+    animeMap, err := s.mapService.CheckForAnimeinMap(ctx, anime)
+    if err == nil {
+        anime.MALId = animeMap.Malid
+        anime.EpisodeNum = animeMap.CalculateEpNum(anime.EpisodeNum)
+        return s.updateAndStore(ctx, anime, anime.UpdateWatchStatus)
+    }
+
+    if anime.SeasonNum == 1 {
+        return s.updateFromDBAndStore(ctx, anime, anime.UpdateWatchStatus)
+    }
+
+    return err
+}
+
+func (s *service) updateAndStore(ctx context.Context, anime *domain.AnimeUpdate, updateFunc func(context.Context, *mal.Client) error) error {
+    client, err := s.malauthService.GetMalClient(ctx)
+    if err != nil {
+        return err
+    }
+
+    if err := updateFunc(ctx, client); err != nil {
+        return err
+    }
+
+    return s.Store(ctx, anime)
+}
+
+func (s *service) updateFromDBAndStore(ctx context.Context, anime *domain.AnimeUpdate, updateFunc func(context.Context, *mal.Client) error) error {
+    req := &domain.GetAnimeRequest{
+        IDtype: anime.SourceDB,
+        Id:     anime.SourceId,
+    }
+
+    animeFromDB, err := s.animeService.GetByID(ctx, req)
+    if err != nil {
+        return err
+    }
+
+    anime.MALId = animeFromDB.MALId
+    return s.updateAndStore(ctx, anime, updateFunc)
+}
+
